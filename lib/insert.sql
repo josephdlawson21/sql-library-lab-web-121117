516b6187d44cb10ca66f6926d890c735f6d3f906
josephdlawson21 <josephdlawson21@gmail.com>
2017-12-20 09:15:14 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO series(title, author_id, subgenre_id) VALUES ("lul",23,25),("lul",23,25);
+
+INSERT INTO books(title, year, series_id) VALUES ("Cool book",1992,232),("Cool book",1992,232),("Cool book",1992,232),("Cool book",1992,232),("Cool book",1992,232),("Cool book",1992,232);
+
+INSERT INTO characters(name,species,motto,series_id,author_id) VALUES("akdjf","lksdf","aslkdfj",32,234),("akdjf","lksdf","aslkdfj",32,234),("akdjf","lksdf","aslkdfj",32,234),("akdjf","lksdf","aslkdfj",32,234),("akdjf","lksdf","aslkdfj",32,234),("akdjf","lksdf","aslkdfj",32,234),("akdjf","lksdf","aslkdfj",32,234),("akdjf","lksdf","aslkdfj",32,234);
+
+INSERT INTO subgenres(name) VALUES("lkdf"),("lkdf");
+
+INSERT INTO authors(name) VALUES("kaldjf"),("lkajdsf");
+
+INSERT INTO character_books(book_id,character_id) VALUES(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341),(42134,3412341);
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "select books.title, books.year from books where books.series_id = 1 order by books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "select characters.name, characters.motto from characters order by characters.motto limit 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select characters.species, count(characters.species) as count from characters group by characters.species order by count desc limit 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "select authors.name, subgenres.name from ((authors inner join series on authors.id = series.author_id)series inner join subgenres on series.subgenre_id = subgenres.id)"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "select  series.title from characters inner join series on characters.series_id = series.id group by series.title having characters.species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "select characters.name, count(character_books.character_id) as series_count from characters inner join character_books on characters.id = character_books.character_id group by characters.id order by series_count desc, characters.name"
 end
